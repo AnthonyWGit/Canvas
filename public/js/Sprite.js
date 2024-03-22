fd863b7f8ecabbd491586455078ebc626c8976a7
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-03-22 20:25:28 +0100

    using utils conversion

--- public/js/Sprite.js
+++ public/js/Sprite.js
@@ -21,8 +21,8 @@ class Sprite {
     }
 
     draw(ctx) {
-        const x = this.gameObject.x * 48
-        const y = this.gameObject.y * 48
+        const x = this.gameObject.x
+        const y = this.gameObject.y
 
         this.isLoaded && ctx.drawImage(this.image,
             0, 0,
