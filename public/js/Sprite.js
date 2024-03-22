334ef05e297938017402d8eab2cef269939c0745
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-03-22 17:34:32 +0100

    correcting brackets

--- public/js/Sprite.js
+++ public/js/Sprite.js
@@ -1,37 +1,34 @@
-class Sprite{
-    constructor(config)
-    {
+class Sprite {
+    constructor(config) {
         //set up the image 
-this.image = new Image()
-this.image.src = config.src
-this.image.onload = () => 
-{
-    this.isLoaded = true
-}
+        this.image = new Image()
+        this.image.src = config.src
+        this.image.onload = () => {
+            this.isLoaded = true
+        }
 
-        //config aniations & initial state
+        //config animations & initial state
         this.animations = config.animations || {
-            idleDown : [
-                [0,0]
+            idleDown: [
+                [0, 0]
             ],
         }
+        this.currentAnimation = config.currentAnimation || "idleDown"
+        this.currentAnimationFrame = 0
+
+        //references the gameObject
+        this.gameObject = config.gameObject
     }
-    this.currentAnimation = config.currentAnimation || "idleDown"
-    this.currentAnimationFrame = 0
 
-    //references the gameObject
-    this.gameObject = config.gameObject
-
-    draw(ctx)
-    {
+    draw(ctx) {
         const x = this.gameObject.x * 48
         const y = this.gameObject.y * 48
 
-        ctx.drawImage(this.image,
-            0,0,
-            48,48,
-            x,y,
-            48,48
+        this.isLoaded && ctx.drawImage(this.image,
+            0, 0,
+            48, 48,
+            x, y,
+            48, 48
         )
     }
 }
