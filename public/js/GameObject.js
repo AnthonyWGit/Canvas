6d97183f08a61fad35814060723b8d9223ca2dd3
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-03-24 10:58:11 +0100

    giving a direction and update method to gameobject

--- public/js/GameObject.js
+++ public/js/GameObject.js
@@ -4,11 +4,17 @@ class GameObject
     {
         this.x = config.x || 0 //defaults to 0 or if falsy value 
         this.y = config.y || 0 
+        this.direction = config.direction || "down"
         this.sprite = new Sprite(
             {
                 gameObject : this,
                 src : config.src || "./public/img/hero.png" , //if no defaults has been set 
             }
         );
+    }
+
+    update()
+    {
+
     }
 }
