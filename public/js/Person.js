5b66aa174d1568fa62eba53e2ff367da5d8a7d83
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-03-24 12:02:03 +0100

    introducing speed property

--- public/js/Person.js
+++ public/js/Person.js
@@ -2,7 +2,9 @@ class Person extends GameObject
 {
  constructor(config){
     super(config) //config of GameObject
-    this.movingProgressRemaining = 48 //using that so people can't be in between grid 
+    this.movingProgressRemaining = 48 //using that so people can't be in between grid && is a speed controller
+    this.speed = 4 // must be a multiple of 2 
+    this.isPlayerControlled = config.isPlayerControlled || false
 
     this.directionUpdate = {
         "up" : ["y", -1],
@@ -14,7 +16,7 @@ class Person extends GameObject
 
  update(state){
     this.updatePosition()
-    if (this.movingProgressRemaining === 0 && state.arrow){
+    if (this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow){
         this.direction = state.arrow
         this.movingProgressRemaining = 48
     }
@@ -24,8 +26,8 @@ class Person extends GameObject
     if (this.movingProgressRemaining > 0)
     {
         const [property,change] = this.directionUpdate[this.direction]  // can work because it exists in GameObjects
-        this[property] += change
-        this.movingProgressRemaining -= 1
+        this[property] += change * this.speed //or just change
+        this.movingProgressRemaining -= this.speed // or just 1 if no speed 
     }
  }
 }
