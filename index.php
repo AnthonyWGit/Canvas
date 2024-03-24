3bfacbd31fefa4e8f9e5463627c4aa9257bd4ad3
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-03-24 10:59:41 +0100

    including person

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 <script src="./public/js/utils.js"></script>
 <script src="./public/js/Overworld.js"></script>
 <script src="./public/js/GameObject.js"></script>
+<script src="./public/js/Person.js"></script>
 <script src="./public/js/Sprite.js"></script>
 <script src="./public/js/OverworldMap.js"></script>
 <script src="./public/js/init.js"></script>
