d103feae24e63ca6ba64c87fd8d1417849804d00
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-03-26 12:02:32 +0100

    creation all animation list and animation methods

--- public/js/Sprite.js
+++ public/js/Sprite.js
@@ -9,26 +9,71 @@ class Sprite {
 
         //config animations & initial state
         this.animations = config.animations || {
-            "idle-down": [
-                [0, 0]
-            ],
+            "idle-down": [[1, 0]],
+            "idle-right": [[1, 3]],
+            "idle-left": [[1, 2]],
+            "idle-up": [[1, 4]],
+            "walk-down" : [[0,0],[1,0],[2,0],[1,0]],
+            "walk-right" : [[0,3],[1,3],[2,3],[1,3]],
+            "walk-left" : [[0,2],[1,2],[2,2],[1,2]],
+            "walk-up" : [[0,4],[1,4],[2,4],[1,4]],
         }
-        this.currentAnimation = config.currentAnimation || "idle-down"
+        this.currentAnimation = "walk-left" //config.currentAnimation || "idle-down"
         this.currentAnimationFrame = 0
 
+        this.animationFrameLimit = config.animationFrameLimit || 48 //Increase = animation is slower ; decrase animation is faster
+        this.animationFrameProgress = this.animationFrameLimit
         //references the gameObject
         this.gameObject = config.gameObject
+    }
+
+    get frame() //getting to see what animation where are in now
+    {
+        return this.animations[this.currentAnimation][this.currentAnimationFrame]
+    }
+
+    setAnimation(key)
+    {
+        if(this.currentAnimation !== key)
+        {
+            this.currentAnimation = key
+            this.currentAnimationFrame = 0
+            this.animationFrameProgress = this.animationFrameLimit
+        }
+    }
+
+    updateAnimationProgress()
+    {
+        //down tick frame progress
+        if (this.animationFrameProgress > 0)
+        {
+            this.animationFrameProgress -= 1
+            return
+        }
+
+        //reset counter
+        this.animationFrameProgress = this.animationFrameLimit
+        this.currentAnimationFrame += 1
+
+        if (this.frame === undefined)
+        {
+            this.currentAnimationFrame = 0
+        }
     }
 
     draw(ctx) {
         const x = this.gameObject.x
         const y = this.gameObject.y
 
+        const [frameX, frameY] = this.frame
+        
         this.isLoaded && ctx.drawImage(this.image,
-            48, 0,
+            frameX * 48, frameY * 48,
             48, 48,
             x, y,
             48, 48
         )
+
+        this.updateAnimationProgress()
     }
 }
