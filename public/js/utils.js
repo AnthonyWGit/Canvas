a05e88a4af70e6d4de12de5a78479c1ded251101
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-03-26 20:28:12 +0100

    nextpos and grid coordinates conv

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,5 +1,28 @@
 const utils = {
         withGrid(n) {
             return n * 48
+        },
+        asGridCoord(x,y)
+        {
+            return `${x*48},${y*48}`
+        },
+        nextPosition(initialX,initialY,direction)
+        {
+            let x = initialX
+            let y = initialY
+            const size = 48
+            if (direction === "left"){
+                x -= size
+            }
+            else if (direction === "right"){
+                x += size
+            }
+            else if (direction ==="up"){
+                y -= size
+            }
+            else if (direction === "down"){
+                y += size
+            }
+            return {x,y}
         }
 }
