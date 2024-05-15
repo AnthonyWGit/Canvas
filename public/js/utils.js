bd69e95890c2af8543270b1d391d99460b125b96
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-05-15 17:01:03 +0200

    added a util to do pixel -> abs coordinates

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,47 +1,51 @@
-const utils = {
-        withGrid(n) {
-            return n * 48
-        },
-        asGridCoord(x,y)
-        {
-            return `${x*48},${y*48}`
-        },
-        nextPosition(initialX,initialY,direction)
-        {
-            let x = initialX
-            let y = initialY
-            const size = 48
-            if (direction === "left"){
-                x -= size
+    const utils = {
+            withGrid(n) {
+                return n * 48
+            },
+            asGridCoord(x,y)
+            {
+                return `${x*48},${y*48}`
+            },
+            asAbsCoord(x,y)
+            {
+                return `${Math.floor(x/48)},${Math.floor(y/48)}`
+            },
+            nextPosition(initialX,initialY,direction)
+            {
+                let x = initialX
+                let y = initialY
+                const size = 48
+                if (direction === "left"){
+                    x -= size
+                }
+                else if (direction === "right"){
+                    x += size
+                }
+                else if (direction ==="up"){
+                    y -= size
+                }
+                else if (direction === "down"){
+                    y += size
+                }
+                return {x,y}
+            },
+            nextPositionUngrid(initialX,initialY,direction)
+            {
+                let x = initialX
+                let y = initialY
+                const size = 1
+                if (direction === "left"){
+                    x -= size
+                }
+                else if (direction === "right"){
+                    x += size
+                }
+                else if (direction ==="up"){
+                    y -= size
+                }
+                else if (direction === "down"){
+                    y += size
+                }
+                return {x,y}
             }
-            else if (direction === "right"){
-                x += size
-            }
-            else if (direction ==="up"){
-                y -= size
-            }
-            else if (direction === "down"){
-                y += size
-            }
-            return {x,y}
-        },
-        nextPositionUngrid(initialX,initialY,direction)
-        {
-            let x = initialX
-            let y = initialY
-            const size = 1
-            if (direction === "left"){
-                x -= size
-            }
-            else if (direction === "right"){
-                x += size
-            }
-            else if (direction ==="up"){
-                y -= size
-            }
-            else if (direction === "down"){
-                y += size
-            }
-            return {x,y}
-        }
-}
+    }
